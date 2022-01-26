1bb231117e6514da0605a365d3c3a8e058ac67a2
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-01-26 12:20:46 +0600

    static middleware used for serving static files

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-// // everything runs from top to bottom. so logger function is called first and then the route is called.
-// // we can use middleware in individual routes also. or we have to put the middleware below.
-// app.use(logger);
+// middleware for serving static files like html, css etc. just go to the route and see the file served.
+// public is the naming convention for the folder where the static files are stored. but we can use anything we want.
+app.use(express.static('public'));
 
 // server running on port 3000
 app.listen(3000)
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 
 // we can use middleware in individual routes also. and as many middleware as we want. 
 // as any many time the same middleware can be used also.
-app.get('/', logger, logger, logger, (req, res) => {
+app.get('/', (req, res) => {
     console.log('Hello World');
     // default folder for ejs is views. then we define the file name i;e index.ejs. 
     // then pass the data to the ejs file like- {text: "from server.js"} using render method.
@@ -26,9 +26,3 @@ const usersRouter = require('./routes/users');
 
 // 1st argument is the path, 2nd argument is the router. all routes in users.js will start with /users
 app.use('/users', usersRouter);
-
-// logger function is middleware. it runs in the middle of user sent the request and the response is sent back to the user.
-function logger(req, res, next){
-    console.log(req.originalUrl);
-    next();
-};
