37c541b1c6e51cf40961b36cf74da7bcd6342d3d
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-01-26 11:08:19 +0600

    EJS view enjine setup in express

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+// Set the view engine to ejs
+app.set("view engine", "ejs");
+
 app.listen(3000)
 
-app.get('/:id', (req, res) => {
-    res.send(`Hello World ${req.params.id}`);
+app.get('/', (req, res) => {
+    console.log('Hello World');
+    // default folder for ejs is views. then we define the file name i;e index.ejs. 
+    // then pass the data to the ejs file like- {text: "from server.js"} using render method.
+    res.render("index", {text: "from server"});
     }
 );
