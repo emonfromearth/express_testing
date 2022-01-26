260a9d1b642bc8d0eac92849834eb6143020e3bb
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-01-26 13:17:05 +0600

    post data then redirect and some refractoring

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,29 @@ router.get("/", (req, res) => {
 });
 
 router.get("/new", (req, res) => {
-  // rendered a from from static/views/users/new.ejs and then posted the data to the server.
+  // rendered a from from static/views/users/new.ejs and then posted the data to the server. 
+  // second parameter {firstname: "your name here"} is the data to be passed to the ejs file.
   res.render("users/new", {firstname: "your name here"});
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body.firstname);
-  // show the data from previous submitted from
-  res.send(`User created with name ${req.body.firstname}`);
+  const isValid = false;
+  if (isValid) {
+    users.push({firstname: req.body.firstname});
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log('Error');
+    // second parameter {firstname: req.body.firstname} will be populate again in the form after error occurs. a good practice for UX.
+    res.render("users/new", {firstname: req.body.firstname});
+  }
+
 });
 
 router
   .route("/:id")
   .get((req, res) => {
-    res.send(`User data ${req.user}`);
+    console.log(req.user);
+    res.send(`User data ${req.params.id}`);
   })
   .put((req, res) => {
     res.send(`User update ${req.params.id}`);
